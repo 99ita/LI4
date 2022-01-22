ebb06456cba2ae6251d1051a28d88a19d791e7dc
Ricardo Machado <yryn150200@gmail.com>
2022-01-22 23:20:18 +0000

    Create Hotel.java

---
+++ src/Model/Data/Hotel.java
@@ -0,0 +1,58 @@
+package Model.Data;
+
+public class Hotel {
+    public Hotel(Location location, String descricao, String restauracao, int lotacao, int gostos) {
+        this.location = location;
+        Descricao = descricao;
+        Restauracao = restauracao;
+        Lotacao = lotacao;
+        this.gostos = gostos;
+    }
+
+   private Location location;
+   private String Descricao;
+   private String Restauracao;
+   private int Lotacao;
+   private int gostos;
+
+    public Location getLocation() {
+        return location;
+    }
+
+
+    public String getDescricao() {
+        return Descricao;
+    }
+
+    public String getRestauracao() {
+        return Restauracao;
+    }
+
+    public int getLotacao() {
+        return Lotacao;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public void setDescricao(String descricao) {
+        Descricao = descricao;
+    }
+
+    public void setRestauracao(String restauracao) {
+        Restauracao = restauracao;
+    }
+
+    public void setLotacao(int lotacao) {
+        Lotacao = lotacao;
+    }
+
+    public void setGostos(int gostos) {
+        this.gostos = gostos;
+    }
+
+    public int getGostos() {
+        return gostos;
+    }
+}
